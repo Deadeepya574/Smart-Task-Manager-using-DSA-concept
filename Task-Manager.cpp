1e61c5a194602814913b82d9ddb3d349f9b38352
Deadeepya574 <deadeepya574@gmail.com>
2026-09-21 21:27:18 +0530

    Auto-update

--- Task-Manager.cpp
+++ Task-Manager.cpp
@@ -17,7 +17,7 @@ class TaskManager
 {
 private:
     vector<Task> tasks;
-    priority_queue<Task,vector<Task>,ComparePriority
+    priority_queue<Task,vector<Task>,ComparePriority> priorityTasks;
     int nextnum = 1;
 
 public:
